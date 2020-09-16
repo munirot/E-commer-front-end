a072bc755c15229d88dd2979a5f5241937c48382
MeakLeangheng <meakleangheng@gmail.com>
2020-09-16 19:14:39 +0700

    Modified bestSeller

--- components/bestSeller/script.js
+++ components/bestSeller/script.js
@@ -27,13 +27,108 @@ export default {
         {
           sold:"760 Sold",
           name:"PS-69",
-          price:"$940.89",
-          oldPrice:"$1029.00",
-          img: require("../../assets/image/p1.webp"),
+          price:"$35.88",
+          oldPrice:"$50.00",
+          img: require("../../assets/image/w5.jpg"),
+        },
+      ],
+      ///New Arrivals
+      listImage1:[
+        {
+          sold:"60 Sold",
+          name:"T-S63",
+          price:"$22.36",
+          oldPrice:"$24.44",
+          img: require("../../assets/image/s2.webp"),
+        },
+        {
+          sold:"460 Sold",
+          name:"AU 63",
+          price:"$2000.89",
+          oldPrice:"$2100.59",
+          img: require("../../assets/image/r1.jpg"),
+        },
+        {
+          sold:"470 Sold",
+          name:"PS-63",
+          price:"$77.99",
+          oldPrice:"$88.79",
+          img: require("../../assets/image/bag1.webp"),
+        },
+        {
+          sold:"760 Sold",
+          name:"PS-69",
+          price:"$10.91",
+          oldPrice:"$16.31",
+          img: require("../../assets/image/h1.webp"),
+        },
+      ],
+      //Best Seller
+      listImage2:[
+        {
+          sold:"60 Sold",
+          name:"T-S63",
+          price:"$63.96",
+          oldPrice:"$125.42",
+          img: require("../../assets/image/c2.webp"),
+        },
+        {
+          sold:"460 Sold",
+          name:"AU 63",
+          price:"$200.89",
+          oldPrice:"$220.59",
+          img: require("../../assets/image/br1.jpeg"),
+        },
+        {
+          sold:"470 Sold",
+          name:"PS-63",
+          price:"$77.99",
+          oldPrice:"$88.79",
+          img: require("../../assets/image/w6.jpg"),
+        },
+        {
+          sold:"760 Sold",
+          name:"PS-69",
+          price:"$35.88",
+          oldPrice:"$50.00",
+          img: require("../../assets/image/bag3.jpg"),
+        },
+      ],
+      //May You Like
+      listImage3:[
+        {
+          sold:"60 Sold",
+          name:"T-S63",
+          price:"$31.76",
+          oldPrice:"$39.76",
+          img: require("../../assets/image/s1.webp"),
+        },
+        {
+          sold:"460 Sold",
+          name:"AU 63",
+          price:"$20.89",
+          oldPrice:"$21.59",
+          img: require("../../assets/image/wa1.jpg"),
+        },
+        {
+          sold:"470 Sold",
+          name:"PS-63",
+          price:"$29.56",
+          oldPrice:"$30.00",
+          img: require("../../assets/image/c1.webp"),
+        },
+        {
+          sold:"760 Sold",
+          name:"PS-69",
+          price:"$190.00",
+          oldPrice:"$200.10",
+          img: require("../../assets/image/su1.jpg"),
         },
       ]
+      ///
     }
   },
+  
   components: {
 
   },
@@ -43,5 +138,5 @@ export default {
   methods: {
     
   }
-  }
-
+      
+    }
